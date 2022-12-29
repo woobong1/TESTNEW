c370fa23ea6234986424ccf54fdb948779ddfa7b
woo <coinus@naver.com>
2022-12-29 18:53:51 +0900

    d
    d

---
+++ Assets/Editor/IOSBuild.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using System;
+using System.IO;
+
+public class IOSBuild
+{
+    private static readonly string defaultPath = "C:/MetaTrendTeam/IOS";
+    private static string folder_date = "";
+    private static string folder_time = "";
+
+}
